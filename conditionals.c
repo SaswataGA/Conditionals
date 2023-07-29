17bf01ffc87ee0fab7aa0d5579869d8949b5c19f
Saswata Ghosal <140143035+SaswataGA@users.noreply.github.com>
2023-07-29 08:21:03 +0600

    Add files via upload

---
+++ conditionals.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int a=10;//Type any integer number you want on here
+    a++;
+    printf("\n%d",a);
+
+    a--;
+    printf("\n%d",a);
+
+    a=a+100;
+    printf("\n%d",a);
+
+    a=a-100;
+    printf("\n%d",a);
+
+    a=a*100;
+    printf("\n%d",a);
+
+    a=a/100;
+    printf("\n%d",a);
+
+
+}
